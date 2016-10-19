ade9afac6d3b2a9b750c483c166dd13fc5082a31
Justin Dierking <justindierking@hardbitsolutions.com>
2016-10-19 15:26:56 -0400

    Delete index.js

--- dev1/dev1/static/js/index.js
+++
@@ -1,8 +0,0 @@
-var app = angular.module('myApp', []);
-app.controller('myCtrl', function($scope, $interval, $http) {
-    $interval(function () {
-            $http.get("getrandom").then(function (response) {
-            $scope.theRandomNumber = response.data;
-        });
-    }, 1000);
-});
