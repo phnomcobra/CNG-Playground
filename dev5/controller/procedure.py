47bcc5ebfd77d02caa7bf374a9a40c8e845666b3
Justin Dierking <justindierking@hardbitsolutions.com>
2016-12-07 16:08:37 -0500

    Add files via upload

--- dev5/controller/procedure.py
+++ dev5/controller/procedure.py
@@ -14,6 +14,7 @@ import json
 
 from ..model.procedure import get_task_grid, \
                               get_related_procedure_grid, \
+                              get_related_procedures, \
                               get_host_grid, \
                               execute
 
@@ -29,6 +30,10 @@ class Procedure(object):
     @cherrypy.expose
     def ajax_get_related_procedure_grid(self, objuuid):
         return json.dumps(get_related_procedure_grid(objuuid))
+    
+    @cherrypy.expose
+    def ajax_get_related_procedures(self, objuuid):
+        return json.dumps(get_related_procedures(objuuid))
     
     @cherrypy.expose
     def ajax_execute_procedure(self, prcuuid, hstuuid):
