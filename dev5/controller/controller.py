e7a652d851382b8063425cff9bba5a68ebb271e7
Justin Dierking <justindierking@hardbitsolutions.com>
2016-12-19 16:31:28 -0500

    Add files via upload

--- dev5/controller/controller.py
+++ dev5/controller/controller.py
@@ -13,7 +13,9 @@ import cherrypy
 import json
 import traceback
 
-from ..model.controller import get_procedure_grid, get_host_grid, get_tiles
+from threading import Thread
+
+from ..model.controller import get_procedure_grid, get_host_grid, get_tiles, execute
 from .messaging import add_message
 
 class Controller(object):
@@ -38,5 +40,21 @@ class Controller(object):
         add_message("controller controller: get controller tiles: {0}".format(objuuid))
         try:
             return json.dumps(get_tiles(objuuid))
+        except Exception:
+            add_message(traceback.format_exc())
+    
+    @cherrypy.expose
+    def ajax_execute_queue(self):
+        add_message("controller controller: execute queue...")
+        
+        cl = cherrypy.request.headers['Content-Length']
+        queue = json.loads(cherrypy.request.body.read(int(cl)))
+        
+        try:
+            session = {}
+            for key, value in cherrypy.session.items():
+                session[key] = value
+            Thread(target = execute, args = (queue, session)).start()
+            return json.dumps({})
         except Exception:
             add_message(traceback.format_exc())
