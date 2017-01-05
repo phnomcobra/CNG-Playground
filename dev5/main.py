acfed66e53b25ba4d9714b8262924e6a96b17d5c
Justin Dierking <justindierking@hardbitsolutions.com>
2017-01-05 15:44:29 -0500

    Created SSL Config

--- dev5/main.py
+++ dev5/main.py
@@ -17,11 +17,25 @@ from .controller.root import Root
 
 def start():
     current_dir = os.path.dirname(os.path.abspath(__file__))
-    cherrypy.config.update({'environment': 'production',
-                            'tools.staticdir.on': True,
+    
+    """
+    cherrypy.config.update({'tools.staticdir.on' : True,
                             'tools.sessions.on' : True,
-                            'tools.auth.on': True,
-                            'tools.staticdir.dir': os.path.join(current_dir, './static'),
-                            'server.socket_host': '0.0.0.0'})
+                            'tools.auth.on' : True,
+                            'tools.staticdir.dir' : os.path.join(current_dir, './static'),
+                            'server.socket_host' : '0.0.0.0',
+                            'server.socket_port' : 443,
+                            'server.ssl_module' : 'ssl',
+                            'server.ssl_certificate' : os.path.join(current_dir, './cert.pem'),
+                            'server.ssl_private_key' : os.path.join(current_dir, './privkey.pem'),
+                            'log.error_file': 'error.log'})
+    """
+    
+    cherrypy.config.update({'tools.staticdir.on' : True,
+                            'tools.sessions.on' : True,
+                            'tools.auth.on' : True,
+                            'tools.staticdir.dir' : os.path.join(current_dir, './static'),
+                            'server.socket_host' : '0.0.0.0',
+                            'log.error_file': 'error.log'})
 
     cherrypy.quickstart(Root())
