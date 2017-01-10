7bb7fcec1f9b15dfcef3a5bfc0ff30edeb24c76a
Justin Dierking <justindierking@hardbitsolutions.com>
2017-01-10 16:15:58 -0500

    Add files via upload

--- dev5/view/index.py
+++ dev5/view/index.py
@@ -10,13 +10,10 @@
 ################################################################################
 
 import jinja2
-import cherrypy
     
 def index_view():
-
     templateLoader = jinja2.FileSystemLoader( searchpath="./dev5/view/templates")
     templateEnv = jinja2.Environment( loader=templateLoader )
     template = templateEnv.get_template('index2.html')
-    
-    return template.render(sessionID = cherrypy.session.id)
+    return template.render()
     
