d6e65500387973a39847bafd1ca2ab3535cda0dc
Justin Dierking <justindierking@hardbitsolutions.com>
2017-01-12 16:07:37 -0500

    Tuning server

--- dev5/main.py
+++ dev5/main.py
@@ -18,24 +18,22 @@ from .controller.root import Root
 def start():
     current_dir = os.path.dirname(os.path.abspath(__file__))
     
-    """
-    cherrypy.config.update({'tools.staticdir.on' : True,
-                            'tools.sessions.on' : True,
-                            'tools.auth.on' : True,
-                            'tools.staticdir.dir' : os.path.join(current_dir, './static'),
-                            'server.socket_host' : '0.0.0.0',
-                            'server.socket_port' : 443,
-                            'server.ssl_module' : 'ssl',
-                            'server.ssl_certificate' : os.path.join(current_dir, './cert.pem'),
-                            'server.ssl_private_key' : os.path.join(current_dir, './privkey.pem'),
-                            'log.error_file': 'error.log'})
-    """
+    config = {
+        'log.screen': False,
+        'tools.staticdir.on' : True,
+        'tools.sessions.on' : True,
+        'tools.sessions.locking' : 'explicit',
+        'tools.auth.on' : True,
+        'tools.staticdir.dir' : os.path.join(current_dir, './static'),
+        'server.thread_pool' : 100,
+        'server.socket_host' : '0.0.0.0',
+        'server.socket_port' : 443,
+        'server.ssl_module' : 'builtin',
+        'server.ssl_certificate' : os.path.join(current_dir, './cert.pem'),
+        'server.ssl_private_key' : os.path.join(current_dir, './privkey.pem')
+        #'log.error_file': 'error.log'
+    }
     
-    cherrypy.config.update({'tools.staticdir.on' : True,
-                            'tools.sessions.on' : True,
-                            'tools.auth.on' : True,
-                            'tools.staticdir.dir' : os.path.join(current_dir, './static'),
-                            'server.socket_host' : '0.0.0.0',
-                            'log.error_file': 'error.log'})
-
+    cherrypy.config.update(config)
+    
     cherrypy.quickstart(Root())
